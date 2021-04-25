2460ec14666fe02dda184857a6089e5007cc8051
Luba Pecheneva <lp5786766@gmail.com>
2021-04-25 15:25:13 -0400

    x is successfully appending to the field

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-$('.field').on('click', function (event) {
-  alert('clicked!');
-});
+// $('.field').on('click', function (event) {
+//   alert('clicked!');
+// });
 
 
 // change background of the field while hover (make different colors later)
@@ -14,4 +14,16 @@ $('.field').hover(
 );
 // Game of tic tac toe
 
-//
+// Adding Xs and Os
+// Rules:
+// 1. Xs go first
+// 2. Players go one by one.
+// 3. Players cannot choose a filled field.
+// 4. Player who gets his tokens in a row of three wins
+
+// Append X on click
+
+$('.field').on('click', function (event) {
+  const x = $('<p>x</p>').addClass("token");
+  $(this).append(x);
+});
