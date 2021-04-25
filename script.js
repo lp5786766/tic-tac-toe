ab5d60f9dfbc53f56a30da6f47c527118f485f26
Luba Pecheneva <lp5786766@gmail.com>
2021-04-25 14:49:14 -0400

    adjusted icons, added hover func

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-
-
 $('.field').on('click', function (event) {
   alert('clicked!');
 });
+
+
+// change background of the field while hover (make different colors later)
+$('.field').hover(
+  function () {
+    $(this).addClass('hover');
+  },
+  function () {
+    $(this).removeClass('hover');
+  }
+);
+// Game of tic tac toe
+
+//
